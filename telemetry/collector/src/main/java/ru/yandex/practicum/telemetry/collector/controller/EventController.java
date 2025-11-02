1ab4490051320e6a38eed9b4caa52aa11b6eed71
AlexanderSharygin <52005798+AlexanderSharygin@users.noreply.github.com>
2025-11-02 12:22:22 +0300

    Update EventController.java

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/controller/EventController.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/controller/EventController.java
@@ -35,23 +35,19 @@ public class EventController extends CollectorControllerGrpc.CollectorController
 
     @Override
     public void collectSensorEvent(SensorEventProto request, StreamObserver<Empty> responseObserver) {
-        log.info("Received sensor event: id={}, payloadType={}", request.getId(),
-                request.getPayloadCase());
+        log.info("Получено событие от датчика с типом: {}.", request.getPayloadCase());
         try {
             if (sensorEventHandlers.containsKey(request.getPayloadCase())) {
                 sensorEventHandlers.get(request.getPayloadCase()).handle(request);
             } else {
-                log.error("No handler found for event type: {}", request.getPayloadCase());
+                log.error("Получено событие от датчика с неизвестным типом: {}", request.getPayloadCase());
                 throw new IllegalArgumentException(
-                        "Cannot find a handler for event type " + request.getPayloadCase());
+                        "Получено событие от датчика с неизвестным типом: " + request.getPayloadCase());
             }
-            log.info("Successfully processed event: id={}", request.getId());
-            // после обработки события возвращаем ответ клиенту
             responseObserver.onNext(Empty.getDefaultInstance());
-            // и завершаем обработку запроса
             responseObserver.onCompleted();
         } catch (Exception e) {
-            log.error("Error while processing event: id={}, error={}", request.getId(), e.getMessage(),
+            log.error("Ошибка во время обработки события датчика с : id={}, Ошибка={}", request.getId(), e.getMessage(),
                     e);
             responseObserver.onError(new StatusRuntimeException(Status.fromThrowable(e)));
         }
@@ -60,20 +56,19 @@ public class EventController extends CollectorControllerGrpc.CollectorController
 
     @Override
     public void collectHubEvent(HubEventProto request, StreamObserver<Empty> responseObserver) {
-        log.info("Received hub event of type: {}.", request.getPayloadCase());
+        log.info("Получено события от хаба с типом: {}.", request.getPayloadCase());
         try {
             if (hubEventHandlers.containsKey(request.getPayloadCase())) {
                 hubEventHandlers.get(request.getPayloadCase()).handle(request);
             } else {
-                log.error("No handler found for the hub event type: {}", request.getPayloadCase());
+                log.error("Получено событие от хаба с неизвестным типом: {}", request.getPayloadCase());
                 throw new IllegalArgumentException(
-                        "Cannot find a handler for the hub event type " + request.getPayloadCase());
+                        "Получено событие от хаба с неизвестным типом: " + request.getPayloadCase());
             }
-            log.info("Successfully processed hub event: id={}", request.getHubId());
             responseObserver.onNext(Empty.getDefaultInstance());
             responseObserver.onCompleted();
         } catch (Exception e) {
-            log.error("Error while processing hub event: hub_id={}, error={}", request.getHubId(), e.getMessage(),
+            log.error("Ошибка во время обработки события хаба с : id={}, Ошибка={}", request.getHubId(), e.getMessage(),
                     e);
             responseObserver.onError(new StatusRuntimeException(Status.fromThrowable(e)));
         }
